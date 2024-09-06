db4a2a990d36f116d20379257f5113f1d732b900
LaraSamara <larasamara2002@gmail.com>
2024-09-06 14:03:46 +0300

    Identity Task

---
+++ Identity/Identity/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(40)]
+        [Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        [Required]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; } = false;
+    }
+}
---
+++ Identity/Identity/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(40)]
+        [Required]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        [Required]
+        public  string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme Password")]
+        [MaxLength(40)]
+        [Required]
+        public string ConfirmedPassword { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(40)]
+        [Required]
+        public string Phone {  get; set; }
+    }
+}
